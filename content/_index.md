cbaa7017e20b25288d0b07b6f408089622215a30
SN1 <129408098+nathaliedesmangles@users.noreply.github.com>
2025-11-06 17:42:35 -0500

    Premier commit

---
+++ content/_index.md
@@ -0,0 +1,15 @@
++++
+title = "Système d'exploitation Linux"
+date = 2025-11-06T14:32:47-05:00
+draft = true
++++
+
+## Description du cours
+
+L‘étudiante ou l’étudiant sera initié aux principes de base des systèmes d’exploitation libre (Linux) et à l’installation et l’exploitation d’une distribution libre de Linux (p. ex. Fedora, Ubuntu, Redhat, Alma Linux). Pour cela, il devra exploiter l’interface graphique et la puissance de la ligne de commande pour gérer une station de travail, explorer les systèmes de fichiers et le partitionnement des disques, gérer les processus, l’automatisation des tâches et la programmation de scripts shell. 
+
+
+## Objectifs du cours
+
+
+## Planification du cours et règlements du cégep
---
+++ public/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "Description du cours Objectifs du cours Planification du cours et règlements du cégep",
+    "description": "Description du cours Objectifs du cours Planification du cours et règlements du cégep",
+    "tags": [],
+    "title": "Système d'exploitation Linux",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Système d'exploitation Linux",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Système d'exploitation Linux",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
